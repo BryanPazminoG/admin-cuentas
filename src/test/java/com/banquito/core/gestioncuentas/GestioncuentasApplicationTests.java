ca1e8067fe97ddae39b57bc92a4f566d91a67fbc
Jacqueline <jacquigavi921@gmail.com>
2024-03-14 00:48:18 -0500

    creacion de proyecto

---
+++ src/test/java/com/banquito/core/gestioncuentas/GestioncuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquito.core.gestioncuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
